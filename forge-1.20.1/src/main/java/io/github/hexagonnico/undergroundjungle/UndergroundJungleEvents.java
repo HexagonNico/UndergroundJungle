4a5c84a3e05b2eea751ef8314478cb4927e72c76
Nico <nico.hex6@gmail.com>
2023-08-11 21:18:33 +0200

    Added 1.20 module

---
+++ forge-1.20.1/src/main/java/io/github/hexagonnico/undergroundjungle/UndergroundJungleEvents.java
@@ -0,0 +1,46 @@
+package io.github.hexagonnico.undergroundjungle;
+
+import io.github.hexagonnico.undergroundjungle.renderers.TempleChestRenderer;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class UndergroundJungleEvents {
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public void creativeTabEvent(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey().equals(CreativeModeTabs.BUILDING_BLOCKS)) {
+            event.accept(UndergroundJungleItems.TEMPLE_BRICKS::get);
+            event.accept(UndergroundJungleItems.CRACKED_TEMPLE_BRICKS::get);
+            event.accept(UndergroundJungleItems.MOSSY_TEMPLE_BRICKS::get);
+            event.accept(UndergroundJungleItems.CHISELED_TEMPLE_BRICKS::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_TILES::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_STAIRS::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_SLAB::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_WALL::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_TILE_STAIRS::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_TILE_SLAB::get);
+            event.accept(UndergroundJungleItems.TEMPLE_BRICK_TILE_WALL::get);
+        } else if(event.getTabKey().equals(CreativeModeTabs.NATURAL_BLOCKS)) {
+            event.accept(UndergroundJungleItems.JUNGLE_GRASS::get);
+            event.accept(UndergroundJungleItems.MUSHROOM_GRASS::get);
+            event.accept(UndergroundJungleItems.JUNGLE_VINES::get);
+            event.accept(UndergroundJungleItems.GLOWING_MUSHROOM::get);
+            event.accept(UndergroundJungleItems.GLOWING_MUSHROOM_BLOCK::get);
+            event.accept(UndergroundJungleItems.MUSHROOM_TALL_GRASS::get);
+            event.accept(UndergroundJungleItems.MUSHROOM_VINES::get);
+        } else if(event.getTabKey().equals(CreativeModeTabs.FUNCTIONAL_BLOCKS)) {
+            event.accept(UndergroundJungleItems.TEMPLE_CHEST::get);
+        } else if(event.getTabKey().equals(CreativeModeTabs.TOOLS_AND_UTILITIES)) {
+            event.accept(UndergroundJungleItems.TEMPLE_KEY::get);
+        }
+    }
+
+    @SubscribeEvent
+    @SuppressWarnings("unused")
+    public void registerRenders(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerBlockEntityRenderer(UndergroundJungleBlockEntities.TEMPLE_CHEST.get(), TempleChestRenderer::new);
+    }
+}
---
+++ forge-1.20.1/src/main/java/io/github/hexagonnico/undergroundjungle/UndergroundJungleMod.java
@@ -0,0 +1,21 @@
+package io.github.hexagonnico.undergroundjungle;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(UndergroundJungleMod.ID)
+public class UndergroundJungleMod {
+
+    public static final String ID = "underground_jungle";
+
+    public UndergroundJungleMod() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        UndergroundJungleBlocks.register(eventBus);
+        UndergroundJungleBlockEntities.register(eventBus);
+        UndergroundJungleItems.register(eventBus);
+        eventBus.register(new UndergroundJungleEvents());
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
